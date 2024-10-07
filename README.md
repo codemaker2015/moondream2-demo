36cf1bc06c5cec64d614f7d2eadf12fd7765a1ef
Vishnu <codemaker2015@gmail.com>
2024-10-08 00:06:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Moondream2 demo
+
+Moondream2 is an open-source, compact vision-language model designed for low-resource environments. With 1.86 billion parameters, it offers a balance between efficiency and performance, making it suitable for various vision-language tasks such as image captioning, visual question answering, visual knowledge reasoning, visual commonsense reasoning, and text recognition.
+
+![demo](demos/demo.gif)
+
+## How to run
+
+In order to run your Local Generative AI Search (given you have sufficiently string machine to run Llama3), you need to 
+download the repository:
+
+````
+git clone https://github.com/codemaker2015/moondream2-demo.git
+````
+You will need to install all the requirements:
+```commandline
+pip install -r requirements.txt
+```
+You can run the app.py using the following command:
+```commandline
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,51 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from PIL import Image
+
+model_id = "vikhyatk/moondream2"
+revision = "2024-08-26"
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, trust_remote_code=True, revision=revision
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
+
+
+# 1. Image Captioning
+# image = Image.open('./inputs/street.jpg')
+# enc_image = model.encode_image(image)
+# input = "Describe the image."
+# output = model.answer_question(enc_image, input, tokenizer)
+# print(f"\nInput: {input}\nOutput: {output}")
+
+
+# 2. Visual Question-Answering
+# image = Image.open('./inputs/cats.jpg')
+# enc_image = model.encode_image(image)
+# input = "How many cats the girl is holding?"
+# output = model.answer_question(enc_image, input, tokenizer)
+# print(f"\nInput: {input}\nOutput: {output}")
+# input = "What is their color?"
+# output = model.answer_question(enc_image, input, tokenizer)
+# print(f"\nInput: {input}\nOutput: {output}")
+
+
+# 3. Visual Knowledge Reasoning
+# image = Image.open('./inputs/tajmahal.jpg')
+# enc_image = model.encode_image(image)
+# input = "Tell about the history of this place"
+# output = model.answer_question(enc_image, input, tokenizer)
+# print(f"\nInput: {input}\nOutput: {output}")
+
+
+# 4. Visual Contextual Understanding
+# image = Image.open('./inputs/boy_playing_with_pet.jpg')
+# enc_image = model.encode_image(image)
+# input = "How does the boy feel and why?"
+# output = model.answer_question(enc_image, input, tokenizer)
+# print(f"\nInput: {input}\nOutput: {output}")
+
+# # 5. Text Recognition
+image = Image.open('./inputs/written_quote.jpg')
+enc_image = model.encode_image(image)
+input = "What's written on this piece of paper?"
+output = model.answer_question(enc_image, input, tokenizer)
+print(f"\nInput: {input}\nOutput: {output}")
