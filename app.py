83444a2eb16189a23a644252346e40de98ada52e
Vishnu <codemaker2015@gmail.com>
2024-10-08 00:14:47 +0530

    Initial commit

--- app.py
+++ app.py
@@ -10,38 +10,38 @@ tokenizer = AutoTokenizer.from_pretrained(model_id, revision=revision)
 
 
 # 1. Image Captioning
-# image = Image.open('./inputs/street.jpg')
-# enc_image = model.encode_image(image)
-# input = "Describe the image."
-# output = model.answer_question(enc_image, input, tokenizer)
-# print(f"\nInput: {input}\nOutput: {output}")
+image = Image.open('./inputs/street.jpg')
+enc_image = model.encode_image(image)
+input = "Describe the image."
+output = model.answer_question(enc_image, input, tokenizer)
+print(f"\nInput: {input}\nOutput: {output}")
 
 
 # 2. Visual Question-Answering
-# image = Image.open('./inputs/cats.jpg')
-# enc_image = model.encode_image(image)
-# input = "How many cats the girl is holding?"
-# output = model.answer_question(enc_image, input, tokenizer)
-# print(f"\nInput: {input}\nOutput: {output}")
-# input = "What is their color?"
-# output = model.answer_question(enc_image, input, tokenizer)
-# print(f"\nInput: {input}\nOutput: {output}")
+image = Image.open('./inputs/cats.jpg')
+enc_image = model.encode_image(image)
+input = "How many cats the girl is holding?"
+output = model.answer_question(enc_image, input, tokenizer)
+print(f"\nInput: {input}\nOutput: {output}")
+input = "What is their color?"
+output = model.answer_question(enc_image, input, tokenizer)
+print(f"\nInput: {input}\nOutput: {output}")
 
 
 # 3. Visual Knowledge Reasoning
-# image = Image.open('./inputs/tajmahal.jpg')
-# enc_image = model.encode_image(image)
-# input = "Tell about the history of this place"
-# output = model.answer_question(enc_image, input, tokenizer)
-# print(f"\nInput: {input}\nOutput: {output}")
+image = Image.open('./inputs/tajmahal.jpg')
+enc_image = model.encode_image(image)
+input = "Tell about the history of this place"
+output = model.answer_question(enc_image, input, tokenizer)
+print(f"\nInput: {input}\nOutput: {output}")
 
 
 # 4. Visual Contextual Understanding
-# image = Image.open('./inputs/boy_playing_with_pet.jpg')
-# enc_image = model.encode_image(image)
-# input = "How does the boy feel and why?"
-# output = model.answer_question(enc_image, input, tokenizer)
-# print(f"\nInput: {input}\nOutput: {output}")
+image = Image.open('./inputs/boy_playing_with_pet.jpg')
+enc_image = model.encode_image(image)
+input = "How does the boy feel and why?"
+output = model.answer_question(enc_image, input, tokenizer)
+print(f"\nInput: {input}\nOutput: {output}")
 
 # # 5. Text Recognition
 image = Image.open('./inputs/written_quote.jpg')
